ac8dd23dcd146ad878367b6b349f428192d54730
hugovezina <hugovezina@outlook.com>
2024-01-11 15:40:08 -0500

    Enemy Base in general and pathfinder prefabs

---
+++ Assets/_Game/Entities/Enemy/Enemy/EnemyLogic.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private float life = 5;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        IsBullet enemyScript = other.GetComponent<IsBullet>();
+
+        if (enemyScript != null)
+        {
+            Destroy(other.gameObject);
+            life--;
+            if (life <= 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
