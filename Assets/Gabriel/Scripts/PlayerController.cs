d86d4529345a7b9fdaf53ab1359412879a2cbd86
gabriellaroche <gabriel.laroche29@gmail.com>
2024-01-10 21:20:16 -0500

    add top-down player controller

--- Assets/Gabriel/Scripts/PlayerController.cs
+++ Assets/Gabriel/Scripts/PlayerController.cs
@@ -1,9 +1,39 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
+    [Header("Player Properties")] 
     
+    [SerializeField] private Vector2 velocity = new (7, 7);
+    [SerializeField] private Rigidbody2D body;
+
+    private Vector2 _movement;
+
+    private PlayerInputActionsTopDown _inputActions;
+
+    private void Awake()
+    {
+        _inputActions = new PlayerInputActionsTopDown();
+        _inputActions.Player.Enable();
+    }
+
+    private void Update()
+    {
+        _movement = _inputActions.Player.Move.ReadValue<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+        
+    private void MoveCharacter()
+    {
+        var movement = _movement * velocity * Time.deltaTime;
+        body.MovePosition(body.position + movement);
+    }
 }
