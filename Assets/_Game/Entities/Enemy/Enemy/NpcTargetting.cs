84b5d9bfa4dd63d9040441d460b1677d43fe6888
hugovezina <hugovezina@outlook.com>
2024-01-11 17:37:50 -0500

    Pathfindind on player

--- Assets/_Game/Entities/Enemy/Enemy/NpcTargetting.cs
+++ Assets/_Game/Entities/Enemy/Enemy/NpcTargetting.cs
@@ -1,13 +1,22 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using Pathfinding;
 using UnityEngine;
 
 public class NpcTargetting : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
+        GameObject player = FindObjectOfType<PlayerMovement>().gameObject;
+        AIDestinationSetter destinationSetter = gameObject.GetComponent<AIDestinationSetter>();
+
         
+        if (destinationSetter != null)
+        {
+            Debug.Log("Go to player");
+            destinationSetter.target = player.transform;
+        }
     }
 
     // Update is called once per frame
