90abb55147f6f521fa7569a126ded425afdebc92
SamaelHeaven <samuelparadisinfo@gmail.com>
2024-01-12 10:00:03 -0500

    Added attack sound to boss

--- Assets/_Game/Entities/Boss/GodBanana.cs
+++ Assets/_Game/Entities/Boss/GodBanana.cs
@@ -132,6 +132,8 @@ public class GodBanana : MonoBehaviour
             return;
         }
         
+        GameObject.Find("LaserShoot").GetComponent<AudioSource>().Play();
+        
         _lastShootBananaTime = 0f;
 
         var isTripleShot = (UnityEngine.Random.value < 0.5f);
