595bcbebc82a5ebf69d59ae35e66c06b301c6868
Charles Manning <35235420+PsyAdept@users.noreply.github.com>
2024-01-11 21:55:49 -0500

    feat: add mickey, fix dog sort order and scripts refactor

--- Assets/Manning/Scripts/MickeyController.cs
+++ Assets/Manning/Scripts/MickeyController.cs
@@ -2,17 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DogController : InteractableEntity
+public class MickeyController : InteractableEntity
 {
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         _can_interact = true;
-        _animator.speed = 12f;
     }
 
     private void OnTriggerExit2D(Collider2D collision)
     {
         _can_interact = false;
-        _animator.speed = 2f;
     }
 }
